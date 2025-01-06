56b48a9d53d9b44637f65d4b448856441a4deb65
Danilo Silva <danilo.silva@msn.com>
2025-01-06 15:31:10 -0300

    Adiciona classe Categoria com métodos de validação
    Adição de um novo arquivo `Categoria.cs` que define a classe `Categoria` dentro do namespace `CodePulse.Domain.Entities`.
    A classe `Categoria` herda de `Entity` e possui duas propriedades privadas: `Nome` e `Slug`.
    Adição de um construtor privado para inicializar as propriedades `Nome` e `Slug` com valores padrão.
    Adição de um construtor público que aceita um parâmetro `nome`, valida este parâmetro e inicializa as propriedades `Nome` e `Slug` com base no valor fornecido.
    Adição do método `Atualizar` para atualizar os dados da categoria, validando o novo nome e atualizando as propriedades `Nome` e `Slug`, além de chamar o método `AtualizarDataAlteracao`.
    Adição de um método estático privado `ValidarCampos` para validar se o nome da categoria não é nulo ou vazio, lançando uma exceção `ArgumentException` se a validação falhar.
    Importação do namespace `CodePulse.Domain.ValueObjects` para utilizar a classe `Slug`.

---
+++ API/src/CodePulse/CodePulse.Domain/Entities/Categoria.cs
@@ -0,0 +1,42 @@
+﻿using CodePulse.Domain.ValueObjects;
+
+namespace CodePulse.Domain.Entities;
+
+public class Categoria : Entity
+{
+    public string Nome { get; private set; }
+    public Slug Slug { get; private set; }
+
+    private Categoria()
+    {
+        Nome = default!;
+        Slug = default!;
+    }
+
+    public Categoria(string nome)
+    {
+        ValidarCampos(nome);
+
+        Nome = nome;
+        Slug = Slug.Criar(nome);
+    }
+
+    /// <summary>
+    /// Atualiza os dados da categoria.
+    /// </summary>
+    public void Atualizar(string nome)
+    {
+        ValidarCampos(nome);
+
+        Nome = nome;
+        Slug = Slug.Criar(nome);
+
+        AtualizarDataAlteracao();
+    }
+
+    private static void ValidarCampos(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new ArgumentException("O nome da categoria não pode ser vazio.");
+    }
+}
